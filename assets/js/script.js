cbc66d64f06c5fd04793a01f380c993f83206957
Camilla Sonesson <Camilla_Sonesson@hotmail.com>
2022-04-18 22:31:01 +0000

    start quiz button initial js commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var startBtn = document.getElementById("start-btn");
+startBtn.onclick = function(event) {
+    console.log("button clicked");
+    renderquiz();
+}
+function renderquiz() {
+    //in the body of this function, add elements to the page that are the quiz 
+    console.log("renderquiz is happening");
+}
+
+
