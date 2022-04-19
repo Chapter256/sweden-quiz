3546fa7f9a98971a133cbbad14c70c5f7068cc07
Camilla Sonesson <Camilla_Sonesson@hotmail.com>
2022-04-19 15:23:17 +0000

    modal for "How to play" added for CSS, HTML & JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,3 +128,32 @@ function incrementScore() {
 function incrementWrongAnswer() {
     
 }
+
+// Code from W3 Schools page on modal windows:  https://www.w3schools.com/howto/howto_css_modals.asp
+// Modal window opens when user clicks 'How To Play' button.
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("how-to-play-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
