daab7a21efa7de1082c7fcc5930dbeaad296e2a2
Camilla Sonesson <Camilla_Sonesson@hotmail.com>
2022-05-19 02:48:28 +0000

    more javascript and questions + answers for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,9 +16,10 @@ nextButton.addEventListener("click", () => {
 function startQuiz() {
     startButton.classList.add("hide")
     howtoPlayButton.classList.add("hide")
+    questionContainerElement.classList.remove("hide")
+    nextButton.classList.remove("hide")
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    questionContainerElement.classList.remove("hide")
     setNextQuestion()
 }; 
 
@@ -33,10 +34,123 @@ function showQuestion(question) {
         const button = document.createElement("button")
         button.innerText = answer.text
         button.classList.add("btn")
+        if (answer.correct) {
+          button.dataset.correct = answer.correct
+        }
         button.addEventListener("click", selectAnswer)
         answerButtonsElement.appendChild(button)
     })
+};
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add("hide")
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild) 
+  }
 }; 
+
+function selectAnswer(e) {
+  const selectButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide")
+  } else {
+    startButton.innerText = "Restart"
+    startButton.classList.remove("hide")
+  }
+}; 
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element) 
+  if (correct) {
+    element.classList.add("correct")
+  } else {
+    element.classList.add("wrong")
+  }
+}; 
+
+function clearStatusClass(element) {
+  element.classList.remove("correct")
+  element.classList.remove("wrong")
+}
+
+const questions = [
+  {
+    question: "What is the highest Mountain in Sweden called?", 
+    answers: [
+      { text: "Kebnekaise", correct: true},
+      { text: "Vikingmountain", correct: false},
+      { text: "Big mountain", correct: false},
+      { text: "Mount Everest", correct: false}
+    ]
+  }, 
+  {
+    question: "What languages is Swedish the closest to?", 
+    answers: [
+      { text: "German & Finnish", correct: false},
+      { text: "Scandinavian & German", correct: false},
+      { text: "Finnish & French", correct: false},
+      { text: "Danish & Norweigan", correct: true}
+    ]
+  }, 
+  {
+    question: "What is the highest Mountain in Sweden called?", 
+    answers: [
+      { text: "Kebnekaise", correct: true},
+      { text: "Vikingmountain", correct: false},
+      { text: "Big mountain", correct: false},
+      { text: "Mount Everest", correct: false}
+    ]
+  }, 
+  {
+    question: "What is the national animal of Sweden?", 
+    answers: [
+      { text: "Deer", correct: false},
+      { text: "Brown bear", correct: false},
+      { text: "Eurasian elk", correct: true},
+      { text: "Fox", correct: false}
+    ]
+  }, 
+  {
+    question: "What is the capital of Sweden?", 
+    answers: [
+      { text: "Stockholm", correct: true},
+      { text: "Helsingborg", correct: false},
+      { text: "Malmö", correct: false},
+      { text: "Göteborg", correct: false}
+    ]
+  }, 
+  {
+    question: "", 
+    answers: [
+      { text: "Swedish dollars (SWED)", correct: false},
+      { text: "Swedish crowns (SEK)", correct: true},
+      { text: "Swedish pesos (SEP)", correct: false},
+      { text: "Scandinavian swish (SDW", correct: false}
+    ]
+  }, 
+  {
+    question: "Which of these is not originally Swedish?", 
+    answers: [
+      { text: "H&M", correct: false},
+      { text: "IKEA", correct: false},
+      { text: "APPLE", correct: true},
+      { text: "Volvo", correct: false}
+    ]
+  }
+]
+
+
+
+
+
+
+
 
 // Code from W3 Schools page on modal windows:  https://www.w3schools.com/howto/howto_css_modals.asp
 // Modal window opens when user clicks 'How To Play' button.
